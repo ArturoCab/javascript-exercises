1e2796c0b3c15f39a5693c579b9c692f30d78ab8
Arturo Cabrera <cabrera_a90@hotmail.com>
2026-01-02 23:13:39 -0600

    remove from array

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...param) {
+    param.forEach((value,id)=>{
+
+        if(!arr.includes(value)) return;
+
+        for(let i=0; i<arr.length; i++){
+            if(arr[i]==value){
+                arr.splice(i,1);
+                i--;
+            }
+        }
+    });
+
+    return arr;
 };
 
 // Do not edit below this line
