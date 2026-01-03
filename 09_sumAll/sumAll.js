08354ed0769a486a2d1325db67176fd8fedca098
Arturo Cabrera <cabrera_a90@hotmail.com>
2026-01-02 23:49:16 -0600

    sum all

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+function sum(n){
+    return n*(n+1)/2;
+}
 
+const sumAll = function(a,b) {
+    if(!(Number.isInteger(a) && Number.isInteger(b)))
+        return "ERROR"
+    if(a <0 || b<0)
+        return "ERROR";
+    var n=[a,b];
+    n.sort();
+    return sum(n[1])-sum(n[0]-1);
 };
 
 // Do not edit below this line
