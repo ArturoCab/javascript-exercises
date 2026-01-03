a74c9dcb1acef8aaa44e9c45ee7f3b49d93995ac
Arturo Cabrera <cabrera_a90@hotmail.com>
2026-01-02 21:47:59 -0600

    repeat string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    if(times<0){
+        return "ERROR"
+    }
+    var s="";
+    for(let i=0; i<times; i++){
+        s+=str;
+    }
 
+    return s;
 };
 
 // Do not edit below this line
