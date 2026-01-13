8013fe28d6cf38ce535ca546869c2dd7c98440e5
Arturo Cabrera <cabrera_a90@hotmail.com>
2026-01-13 14:14:24 -0600

    12 caluclator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if(arr.length==0) return 0;
+  let val=arr.reduce((acc, cur)=>{
+    return acc+=cur;
+  },0);
+
+  return val;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if(arr.length==0) return 0;
+  let val=arr.reduce((acc, cur)=>{
+    return acc*=cur;
+  },1);
 
+  return val;
 };
 
-const power = function() {
-	
+const power = function(base,exp) {
+  if(exp==0) return 1;
+  let acc=base;
+  for(let i=1; i<exp; i++){
+    acc*=base;
+  }
+	return acc;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if(a==0) return 1;
+  if(a==1) return 1;
+  let acc=a;
 	
+  for(let iterator=a-1; iterator>0; iterator--){
+    acc*=iterator;
+  }
+  return acc;
 };
 
 // Do not edit below this line
