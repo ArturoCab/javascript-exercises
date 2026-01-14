7210560a8f6a00d1787d35b1efc08bf2e44393de
Arturo Cabrera <cabrera_a90@hotmail.com>
2026-01-14 13:09:49 -0600

    16 fin the oldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    array=array
+    .sort((a,b)=>{
+        let f=new Date().getFullYear();
+        
+        let x=("yearOfDeath" in b)?b.yearOfDeath:f;
+        let y=("yearOfDeath" in a)?a.yearOfDeath:f;
+        
+        return (x-b.yearOfBirth)-(y-a.yearOfBirth);
+    })
+    return array[0];
 };
 
 // Do not edit below this line
