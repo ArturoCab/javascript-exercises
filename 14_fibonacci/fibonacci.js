38c89b0816ae09b7e4cbee28012160711d0fddb1
Arturo Cabrera <cabrera_a90@hotmail.com>
2026-01-14 11:54:43 -0600

    14 fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    console.log({index});
+    index=Number.parseInt(index);
+    console.log({index});
+    if(index<0) return "OOPS";
+    if(index==0) return 0;
+    if(index<=2) return 1;
+    var previous=[1,1];
+    var result;
 
+    for(let i=2; i<index; i++){
+        result=previous[0]+previous[1];
+        previous[0]=previous[1];
+        previous[1]=result;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
